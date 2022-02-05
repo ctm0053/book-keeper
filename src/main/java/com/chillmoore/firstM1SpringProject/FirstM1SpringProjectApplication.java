fb731b3dc6265e953b390b9875ed72705ef02c4f
Christopher Moore <ctm0053@gmail.com>
2022-02-05 11:03:01 -0800

    model creation

---
+++ src/main/java/com/chillmoore/firstM1SpringProject/FirstM1SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.chillmoore.firstM1SpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstM1SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstM1SpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chillmoore/firstM1SpringProject/FirstM1SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chillmoore.firstM1SpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstM1SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
